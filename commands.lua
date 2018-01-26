5ac91c814b523ba69ae5b1adfa96456570540780
Julian Zhou <jzhou2oo6@gmail.com>
2018-01-26 12:17:09 -0600

    Add Version and Pong Commands

--- commands.lua
+++ commands.lua
@@ -130,6 +130,14 @@ local function ping(usr,chan,msg)
 	return "pong"
 end
 add_cmd(ping,"ping",0,"pong",true)
+local function pong(usr,chan,msg)
+	return "ping"
+end
+add_cmd(pong,"pong",0,"ping",true)
+local function version(usr,chan,msg)
+	return "JZTech101's fork of Crackbot"
+end
+add_cmd(version,"version",0,"version",true)
 local function source(usr, chan, msg)
 	return "https://github.com/jztech101/Crackbot"
 end
