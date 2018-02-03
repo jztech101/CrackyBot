ba810486483eb0aef716a8c9148edd210131ad84
Julian Zhou <jzhou2oo6@gmail.com>
2018-02-03 02:27:13 +0000

    Moved some commands to randcmds

--- commands.lua
+++ commands.lua
@@ -126,18 +126,6 @@ end
 add_cmd(suicide,"suicide",101,"Quits the bot",true,{"quit","die"})
 
 --PING
-local function ping(usr,chan,msg)
-	return "pong"
-end
-add_cmd(ping,"ping",0,"pong",true)
-local function pong(usr,chan,msg)
-	return "ping"
-end
-add_cmd(pong,"pong",0,"ping",true)
-local function shrug(usr, chan, msg)
-        return "┻━┻ ︵ ¯\_(ツ)_/¯ ︵ ┻━┻"
-end
-add_cmd(shrug, "shrug", 0, "shrug", true)
 local function raw(usr, chan, msg)
         ircSendRawQ(msg)
 end
@@ -160,21 +148,6 @@ local function sneaky2(usr,chan,msg)
 	return nil
 end
 add_cmd(sneaky2,"./moo",0,nil,false)
-local function sneaky3(usr,chan,msg)
-	return "MooOoOoooOooo"
-end
-add_cmd(sneaky3, "moo",0,nil, false)
-local function potato(usr, chan, msg) 
-ircSendChatQ(chan, "\001ACTION is a potato", false)
-end
-add_cmd(potato, "potato", 0, nil, false)
-
-local function cookie(usr,chan,msg)
-nick = usr.nick
-if not (msg == "" or msg == nil) then nick = msg end
-ircSendChatQ(chan, "\001ACTION gives " + nick +" a cookie\001", false) 
-end
-add_cmd(cookie,"cookie",0,false,false)
 local function act(usr,chan,msg)
 	if msg then 
 		msg2 = msg
