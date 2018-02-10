249b04705d21307c665be5578a1c3473203c9a84
Julian Zhou <jzhou2oo6@gmail.com>
2018-02-10 18:18:50 +0000

    Update SSL to tlsv1.2

--- irc/init.lua
+++ irc/init.lua
@@ -107,7 +107,7 @@ function meta_preconnect:connect(_host, _port, _password)
 		if type(secure) == "table" then
 			params = secure
 		else
-			params = {mode = "client", protocol = "sslv23", verify = "none"}
+			params = {mode = "client", protocol = "tlsv1_2", verify = "none"}
 		end
 
 		s = ssl.wrap(s, params)
