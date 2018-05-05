fe07111f591918888da6ae2753a104e46a4c8a42
Julian Zhou <jzhou2oo6@gmail.com>
2018-05-05 20:33:02 +0000

    Oops

--- hooks.lua
+++ hooks.lua
@@ -448,7 +448,9 @@ pcall(irc.unhook, irc, "OnCTCP", "ctcp1")
 irc:hook("OnCTCP","ctcp1",onCTCP)
 
 local function onInvite(usr, msg)
-       ircSendChatQ(config.logchannel, "[Invite] ".. tostring(usr.nick.."!"..usr.username.."@"..usr.host) .. ": " .. tostring(msg))
+       if config.logchannel then
+               ircSendChatQ(config.logchannel, "[Invite] ".. tostring(usr.nick.."!"..usr.username.."@"..usr.host) .. ": " .. tostring(msg))
+       end
 end
 pcall(irc.unhook, irc, "OnInvite", "invite1")
 irc:hook("OnInvite","invite1", onInvite)
